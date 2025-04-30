b57a06afe8a2082b05e59878ae9b362094a10952
Kumar Aryan <kumararyanaim@gmail.com>
2025-04-30 20:36:08 +0530

    fixed already been declared

--- frontend/src/components/client/UpdateProfile.jsx
+++ frontend/src/components/client/UpdateProfile.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect } from 'react';
 import api from '../../utils/api';
 import { getUserName, setUserName } from '../../utils/auth';
 
